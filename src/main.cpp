da094e9b15e790c4062e8baa4460e3acce902f1d
Luiz Fernando Silvério Felisbino <kakasten.cipher@gmail.com>
2025-10-04 17:37:22 -0300

    feat: Add non-blocking HID script for launching browser and navigating tabs

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <pico/stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <bsp/rp2040/board.h>
-#include <tusb.h>
 
-#include "usb_descriptors.h"
+#include "PicoKeyboard.hpp"
+#include "KeyboardLayout.hpp"
 
-int main()
-{
-    stdio_init_all();
+PicoKeyboard keyboard;
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) { (void)itf; (void)report_id; (void)report_type; (void)buffer; (void)reqlen; return 0; }
+void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) { (void)itf; (void)report_id; (void)report_type; (void)buffer; (void)bufsize; }
+
+int main(){
+    keyboard.begin();
+    keyboard.set_layout(Layout::US);
+
+    keyboard.task();
+
+    keyboard.type("~");
+    tusb_time_delay_ms_api(500);
+    keyboard.type_line("firefox");
+    tusb_time_delay_ms_api(1000);
+    keyboard.type_line("");
+    tusb_time_delay_ms_api(2000);
+    keyboard.type_line("github.com/kakasten/AuthDuck");
+    tusb_time_delay_ms_api(3000);
+    for(int i = 0; i < 26; i++){
+        keyboard.type("\t");
     }
+    keyboard.type_line("");
+    while (true) {
+        keyboard.task();
+    }
+
+    return 0;
 }
